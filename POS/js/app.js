a4a284b53b8eeddb14c5f6cf94d2205f820808ac
wmoh <wmoh.mail@gmail.com>
2020-10-05 12:04:56 +0100

    upload online - HTML and CSS Only

---
+++ POS/js/app.js
@@ -0,0 +1,67 @@
+/**************************************************************
+ *
+ *                   Project Name :
+ *                   Copyright(c) ™ WMOH 2020.
+ *
+ *
+ *
+ ************************************************************/
+
+
+
+
+
+// Full Screen By W3School
+
+var elem = document.documentElement;
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+
+/// Time
+/* 
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleTimeString(); */
+
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
